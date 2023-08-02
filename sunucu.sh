9a78da77d128fde850305fd87c95bfa826bdda86
tasmuhammed <141078496+tasmuhammed@users.noreply.github.com>
2023-08-02 16:01:10 +0300

    Create sunucu.sh

---
+++ sunucu.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+SAKIR="SAKİR BEY"
+SAKIR+="\nSınırsız İnternet TOOLS"
+SAKIR+="\nVİP PREMİUM TOOLS"
+SAKIR+="\nDEVELOPER TERMUX HACKİNG"
+SAKIR+="\n"
+SAKIR+="\nDATA VERİES"
+SAKIR+="\nSQL HACKİNG"
+MESAJ="\nHACKİN TOOLS KURULUM"
+MESAJ+="\nTR BİZİ TERCİH ETTİĞİNİZ İÇİN TEŞEKKÜRLER TR"
+MESAJ+="\nEN THANK YOU FOR CHOOSING US EN"
+MESAJ+="\n "
+MESAJ+="\n"
+MESAJ+="\nDEVELOPER SAKİRBEY"
+MESAJ+="\nTELEGRAM : @SakirBey1"
+MESAJ+="\nİNSTAGRAM : @developer.rat"
+MESAJ+="\n "
+MESAJ+="\n❗İşlem Bitene Kadar Uygulamayı Terk Etmeyin❗"
+YARDIM="\n❗❗ %50, %70 VE %75'te Durakladığında Y Yazıp Enter Yapınız ❗❗"
+YARDIM+="\n "
+BOSLUK="\n "
+TOOLS="\nVeriler alınıyor..."
+TOOLS+="\nData verileri alındı."
+TOOLS+="\nVeriler DataBase Yükleniyor..."
+TOOLS+="\nVeriler Yüklendi."
+TOOLS+="\nandroid/data"
+TOOLS+="\nDCMI/android/data"
+TOOLS+="\nandroid/data/com.*****/*** {random} \ndata/com.******.*** {newfolder}.rat \ndata/com.****.*** \n NewFolder yeni dosya açılıyor"
+TOOLS+="\nYeni Dosya Açıldı \n Sınırsız İnternet✅ \nSınırsız İnternet✅ \nSınırsız İnternet✅ \nSınırsız İnternet✅ \n Sınırsız İnternet✅ \n Sınırsız İnternet✅"
+TOOLS+="\nCihaza Sınırsız İnternet✅"
+
+clear
+echo -e $SAKIR
+echo -e $YARDIM
+echo -e $BOSLUK
+echo "⏳ TERMUX GEREKSİNİMLERİNİ GÜNCELLİYORUM ⏳"
+echo "⏳ I UPDATE YOUR REQUIREMENTS ⏳"
+echo -e $BOSLUK
+pkg update -y
+clear
+termux/server.check+{a.sh} #server bağlantısı
+clear
+echo -e $SAKIR
+echo -e $BOSLUK
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ CİHAZINIZA PYTHON KURULUYOR ⌛"
+echo "⌛ PYTHON IS INSTALLED ON YOUR DEVICE ⌛"
+echo -e $BOSLUK
+pkg install python3 -y
+pip install --upgrade pip
+clear
+echo -e $SAKIR
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ GIT KURULUYOR ⌛"
+echo "⌛ INSTALLING GIT ⌛"
+echo -e $BOSLUK
+pkg install git -y
+clear
+echo -e $SAKIR
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ TELETHON KURULUYOR ⌛"
+echo "⌛ INSTALLING TELETHON ⌛"
+echo -e $BOSLUK
+pip install telethon
+clear
+echo -e $SAKIR
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ TOOLS İNDİRİYORUM ⌛"
+echo "⌛ I DOWNLOAD THE TOOLS ⌛"
+echo -e $BOSLUK
+
+clear
+echo -e $SAKIR
+echo -e $BOSLUK
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ GEREKSİNİMLERİ KURUYORUM..."
+echo "⌛ INSTALLING REQUIREMENTS..."
+echo -e $BOSLUK
+clear
+ 
+touch Deneme/Sakir.txt
+touch Deneme/Ornek1.txt
+touch Deneme/Ornek2.txt
+
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo -e $TOOLS
+echo "\033[32mDostum Cihazına Sınırsız İnternet✅)\033[0m"
+echo "\033[31m10\033[0m"
+echo "\033[31m20\033[0m"
+echo "\033[31m25\033[0m"
+echo "\033[31m30\033[0m"
+echo "\033[31m35\033[0m"
+echo "\033[31m40\033[0m"
+echo "\033[31m45\033[0m"
+echo "\033[31m50\033[0m"
+echo "\033[31m55\033[0m"
+echo "\033[31m60\033[0m"
+echo "\033[31m65\033[0m"
+echo "\033[31m70\033[0m"
+echo "\033[31m75\033[0m"
+echo "\033[31m80\033[0m"
+echo "\033[31m85\033[0m"
+echo "\033[31m90\033[0m"
+echo "\033[31m95\033[0m"
+echo "\033[31m100\033[0m"
+echo "1\033[31m10000..\033[0m"
+echo "\033[36m▉▉▉▉▉\033[0m"
+echo "\033[36m▉▉▉▉▉▉▉▉▉▉\033[0m"
+echo "\033[36m▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉\033[0m"
+echo "\033[36m▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉\033[0m"
+echo "\033[36m▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉\033[0m"
+echo "\033[36m▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉\033[0m"
+echo "\033[36m▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉\033[0m"
+echo "\033[32mİşlem Tamamlandı✔️\033[0m"
+echo "\033[32mSınırsız İnternet✅\033[0m"
+echo "\033[32mSınırsız İnternet✅\033[0m"
+
+# Dostum kodları çalamazsın çalsanda hacklediğin cihaz benim servere düşer uzaklaş burdan.!
+
+echo "\033[32m╔══════ 💎SakirBey💎═════════╗\033[0m"
+echo "\033[32m║                               ║\033[0m"
+echo "\033[32m║İnstagram : tas_muhammed.01.tr ║\033[0m"
+echo "\033[32m║                               ║\033[0m"
+echo "\033[32m╚══════✨Vip Tools✨═════════╝\033[0m"
